df8457810e80cab32ef73435b3a6b65d71e63da5
yiting_Lee <yi.tinnnng@gmail.com>
2019-12-06 12:16:00 +0800

    12/6第一次上傳

---
+++ FoundNews/src/ConnectURL.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ConnectURL {
+	private String urlStr;
+    
+    public ConnectURL(String urlStr) {
+    	this.urlStr = urlStr;
+	}
+    
+    private String fetchContent() throws IOException{
+		URL url = new URL(this.urlStr);
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	
+		String retVal = "";
+	
+		String line = null;
+		
+		while ((line = br.readLine()) != null){
+		    retVal = retVal + line + "\n";
+		}
+	
+		return retVal;
+    }
+    
+    
+    
+
+}
